742d72c26c4df57c675ca219f02adc28c3135f08
mrk74-2023 <124262652+mrk74-2023@users.noreply.github.com>
2023-02-25 08:15:20 -0800

    Update Calculator.java

--- src/main/java/com/leszko/calculator/Calculator.java
+++ src/main/java/com/leszko/calculator/Calculator.java
@@ -3,6 +3,11 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+/**
+* models
+* added 
+*/
+
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
